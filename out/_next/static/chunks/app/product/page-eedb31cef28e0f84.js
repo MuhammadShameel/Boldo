a81ad669299dc38a7ec12c972c0cec085cc52706
MuhammadShameel <mshameelkz@gmail.com>
2023-06-15 08:19:43 -0700

    out folder added

---
+++ out/_next/static/chunks/app/product/page-eedb31cef28e0f84.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[752,931],{2896:function(e,n,s){Promise.resolve().then(s.t.bind(s,8707,23)),Promise.resolve().then(s.t.bind(s,8920,23))}},function(e){e.O(0,[707,920,253,488,744],function(){return e(e.s=2896)}),_N_E=e.O()}]);
