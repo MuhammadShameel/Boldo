93ac1602bcbc2bff8ab834816f539e1f64dabd8d
MuhammadShameel <mshameelkz@gmail.com>
2023-06-22 11:26:38 -0700

    removes unecessary file

--- app/[pageNO].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const pageNo = () => {
-  const router = useRouter();
-  const pageNumber = router.query.pageNo;
-  return <h1>my {pageNumber} content</h1>;
-};
-
-export default pageNo;
